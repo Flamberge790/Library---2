4aaa628a3667f3532b7aa6a9ab01382c0dc6564e
dawod <dawid.dz4229@gmail.com>
2025-01-20 15:23:29 +0100

    back to "widoki" commit mb chat

---
+++ Library/Migrations/202501201403535_2.cs
@@ -0,0 +1,36 @@
+﻿namespace Library.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CartItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(maxLength: 128),
+                        BookId = c.Int(nullable: false),
+                        DateAdded = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Books", t => t.BookId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId)
+                .Index(t => t.BookId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CartItems", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.CartItems", "BookId", "dbo.Books");
+            DropIndex("dbo.CartItems", new[] { "BookId" });
+            DropIndex("dbo.CartItems", new[] { "UserId" });
+            DropTable("dbo.CartItems");
+        }
+    }
+}
---
+++ Library/Models/Cart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+
+        public int BookId { get; set; }
+        [ForeignKey("BookId")]
+        public virtual Book Book { get; set; }
+
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+    }
+}
